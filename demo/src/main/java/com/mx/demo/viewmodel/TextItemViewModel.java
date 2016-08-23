7b54411a83f721966bda019611b7163a7c74c6d3
刘宇轩 <liuyuxuan@gomeplus.com>
2016-08-23 20:03:05 +0800

    add  pull to refresh recycler view

--- demo/src/main/java/com/mx/demo/viewmodel/TextItemViewModel.java
+++ demo/src/main/java/com/mx/demo/viewmodel/TextItemViewModel.java
@@ -1,20 +1,35 @@
 package com.mx.demo.viewmodel;
 
+import android.util.Log;
+
 import com.mx.demo.BR;
 import com.mx.demo.viewmodel.viewbean.ItemViewBean;
 import com.mx.demo.viewmodel.viewbean.TextItemViewBean;
 import com.mx.framework2.viewmodel.RecyclerItemViewModel;
+import com.mx.framework2.widget.ClickCommand;
 
 /**
  * Created by chenbaocheng on 16/8/14.
  */
 public class TextItemViewModel extends RecyclerItemViewModel<ItemViewBean> {
+    TextItemViewBean item;
+
+    public ClickCommand getClickCommand() {
+        return new ClickCommand() {
+            @Override
+            public void execute(int id) {
+                Log.d("PTR", "ClickCommand==> " + item.getText());
+            }
+        };
+    }
+
     private String textToShow = "default content";
 
     @Override
     protected void onItemChange(ItemViewBean oldItem, ItemViewBean item) {
-        textToShow = ((TextItemViewBean)item).getText();
-        if(((TextItemViewBean)item).isUpperCase()){
+        this.item = (TextItemViewBean) item;
+        textToShow = ((TextItemViewBean) item).getText();
+        if (((TextItemViewBean) item).isUpperCase()) {
             textToShow = textToShow.toUpperCase();
         }
         notifyChange();
