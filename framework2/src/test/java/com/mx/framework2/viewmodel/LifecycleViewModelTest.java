0aba6f081ee2116ecd1439dbc9efc9e9a89ee43c
wangwei-ds10 <wangwei-ds10@yolo24.com>
2016-09-02 14:39:14 +0800

    test

--- framework2/src/test/java/com/mx/framework2/viewmodel/LifecycleViewModelTest.java
+++ framework2/src/test/java/com/mx/framework2/viewmodel/LifecycleViewModelTest.java
@@ -20,6 +20,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
 
 /**
  * Created by wwish on 16/8/30.
@@ -76,6 +77,7 @@ public class LifecycleViewModelTest {
         ViewModelScope viewModelScope=(ViewModelScope) ReflectUtil.invoke(testModel,"getActivity");
 
         assertEquals(viewModelScope,baseActivity);
+        anyInt();
 
     }
 
