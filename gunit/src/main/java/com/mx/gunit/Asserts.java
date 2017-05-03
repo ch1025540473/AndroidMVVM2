1dd93525715c875d0ca7e66516bc1ca53d9fc508
chenbaocheng <chenbaocheng@gomeplus.com>
2017-05-03 18:30:21 +0800

    new module gunit for unit test base codes

---
+++ gunit/src/main/java/com/mx/gunit/Asserts.java
@@ -0,0 +1,10 @@
+package com.mx.gunit;
+
+import org.junit.Assert;
+
+/**
+ * Created by chenbaocheng on 17/5/3.
+ */
+
+public abstract class Asserts extends Assert {
+}
---
+++ gunit/src/main/java/com/mx/gunit/BaseTest.java
@@ -0,0 +1,14 @@
+package com.mx.gunit;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by chenbaocheng on 17/5/3.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class BaseTest extends Asserts {
+    public BaseTest() {
+        super();
+    }
+}
